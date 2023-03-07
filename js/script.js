2b685d7e1f3714bb3307d18f63f6c42edbf05386
giovannitamburini <giovatambu@gmail.com>
2023-03-07 13:18:51 +0100

    added pseudo code

--- js/script.js
+++ js/script.js
@@ -8,3 +8,20 @@ Gestire l'inserimento dei numeri tramite 5 input diversi.
 */
 
 // PSEUDO CODICE
+
+/*
+- creo una funzione che generi un numero casuale per 5 volte
+- creo un bottone che se cliccato attiva la mia funzione
+- visualizzo in pagina i 5 numeri creati
+- faccio partire un timer di 10 secondi
+- allo scadere del timer i numeri scompaiono
+- allo scadere del timer appaino 5 campi di input all'interno dei quali l'utente dovrà inserire i 5 numeri generati
+- devo controllare se i numeri inseriti corrispondono ai numeri generati in precedenza
+- comunico il risultato del gioco:
+  ° SE tutti i numeri inseriti corrispondono ai numeri generati:
+    - comunico all'utente la vittoria
+  ° ALTRIMENTI SE alcuni numeri corrispondono ma non tutti:
+    - comunico all'utente i numeri indovinati
+  ° ALTRIMENTI:
+    - comunico all'utente che non ha indovinato nessun numero
+*/
